8eba5e1feabd640267cb4826a20dadc4aeefe1ff
Simon Koudijs <simonkoudijs@shipitsmarter.com>
2022-12-19 07:46:41 +0100

    Having the valve as a single string so that we can put it into a cached string and starting experiment with a*

--- console/Day16.cs
+++ console/Day16.cs
@@ -1,40 +1,51 @@
+using System.Collections.Concurrent;
 using System.Numerics;
+using System.Text;
 using System.Text.RegularExpressions;
+using EMK.Cartography;
+
 namespace advent_of_code_day16;
 
 public class Day16
 {
     Regex r;
+    
 
     public class Valve
     {
+        public static float xCounter = 0;
         public Valve()
         {
         }
 
-        public Valve(string line, Regex r)
+        public Valve(string line, Regex r, Graph g)
         {
             var result = r.Match(line);              
             Id = result.Groups["v"].Value;
+            var bytes = new byte[]{ (byte)Id[0], (byte)Id[1] };
+            ShortId = Encoding.Unicode.GetString(bytes)[0];
             Rate = int.Parse(result.Groups["fr"].Value);
             OtherValveIds = result.Groups["ov"].Captures.Select(c => c.Value).ToList();
+            Node = g.AddNode(ShortId,0,0);
         }
 
+        public Node Node {get;set;}
+        public char ShortId { get; set; }
         public string Id { get; set; }
         public int Rate { get; set; }
         public List<string> OtherValveIds {get; set;} = new List<string>();
         public List<Valve> OtherValves { get; set; } = new List<Valve>();
 
-        public void Resolve(List<Valve> valves)
+        public void Resolve(List<Valve> valves, Graph g)
         {
             foreach (var id in OtherValveIds)
-                OtherValves.Add(valves.Single(v => v.Id == id));
+            {
+                var valve = valves.Single(v => v.Id == id);
+                OtherValves.Add(valve);
+                g.Add2Arcs(valve.Node, Node, 1);
+            }
         }
-
     }
-
-    private int highestReleasing = 0;
-    private int highestResult = 0;
 
     public Day16()
     {
@@ -44,53 +55,94 @@ public class Day16
 
     public async Task Execute()
     {
-        var lines = await File.ReadAllLinesAsync("console/day16-simple.txt");
-        var valves = lines.Select(l => new Valve(l, r)).ToList();
+        var lines = await File.ReadAllLinesAsync("console/day16.txt");
+        var g = new Graph();
+        var valves = lines.Select(l => new Valve(l, r, g)).ToList();
         foreach (var valve in valves)
-            valve.Resolve(valves);
+            valve.Resolve(valves, g);
 
         var openValves = valves
             .Where(v => v.Rate > 0)
             .OrderByDescending(v => v.Rate)
-            .ToList();
-
-//1235
-// Guess: 2704 -> that is to high?!
-        
+            .ToArray();
+        var openValvesChars = openValves.Select(v => v.ShortId).ToArray();    
+        var openValvesString = new String(openValvesChars, 0, openValves.Length);
+                
         var aa = valves.First(v => v.Id == "AA");
-        var result = Tick(aa, 30, 0, 0, openValves);        
-        System.Console.WriteLine($"part1: {result}");
+
+        // Take the top 5 and pick the closest
+        // Repeat this until we have the cheapset route
+        // We might want to randomize this a little bit.
+
+        var minutesLeft = 30;
+        while (minutesLeft > 0)
+        {
+
+
+
+
+
+        }
+
         
+
+        // Just create all options that we could search
+        // Then go traverse that thing back and forth! Calculate afterwards the costs
+
+        //1235
+        // Guess: 2704 -> that is to high?!
+        
+        var result = 412;
+        System.Console.WriteLine($"part1: {result}");
     }
 
-    public int Tick(Valve valve, int minutesLeft, int releasing, int total, List<Valve> openValves)
+    // So we will only use this to find the shortest route but still be a brutforcing it.
+    public int FindPath(Graph g, Valve start, Valve end)
+    {
+        AStar AS = new AStar(g);
+        AS.SearchPath(start.Node, end.Node);
+        return AS.PathByNodes.Count();  // During the traversal we should actually also decide if we want to open valves! Might be worth it. Or not?
+    }
+
+
+    // We could also save the previous calculations with the same input data. The only thing is that we should not keep minutesleft, total and releasing off course.
+
+    public int Tick(Valve valve, int minutesLeft, int releasing, int total, string openValves)
     {
         minutesLeft--;
-        if (minutesLeft <= 0)
-            return total;
-        
-        total = total + releasing;
         var result = total;
-        if (openValves.Count > 0)
+        if (minutesLeft > 0)
         {
-            if (openValves.Contains(valve))
+            total = total + releasing;
+            result = total;
+            if (openValves.Length > 0)
             {
-                var openValvesMinus1 = openValves.ToList();
-                openValvesMinus1.Remove(valve);
-                result = int.Max(result,
-                    Tick(valve, minutesLeft, releasing + valve.Rate, total, openValvesMinus1));
+                var valvePosition = openValves.IndexOf(valve.ShortId);
+                if (valvePosition > -1) // So we have the valve in our open list
+                {
+                    result = int.Max(result,
+                        Tick(valve, minutesLeft, releasing + valve.Rate, total, openValves.Remove(valvePosition, 1)));
+                }
+
+                
+                foreach (var item in valve.OtherValves)
+                {
+                    result = int.Max(result,
+                        Tick(item, minutesLeft, releasing, total, openValves));
+                }
             }
-            else    // This else might be to much, it could be more effecient to walk by a valve without opening it if the score is high enough on the other one
+            else 
             {
-                result = int.Max(result,
-                    valve.OtherValves.Max(v => Tick(v, minutesLeft, releasing, total, openValves)));
+                // Nothing happens, wait until it's done
+                // We should also be able to calculate when it's done and increase the stuff a bit.
+                result = result + releasing * minutesLeft;
             }
         }
-        else 
+        
+        if (result > highestResult)
         {
-            // Nothing happens, wait until it's done
-            // We should also be able to calculate when it's done and increase the stuff a bit.
-            return result + releasing * minutesLeft;
+            highestResult = result;
+            System.Console.WriteLine("highest result: " + highestResult);
         }
 
         return result;
